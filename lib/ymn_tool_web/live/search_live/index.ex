d7278d079869c0aaefdd4e3b20be86d8f7373854
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-18 22:20:53 +0900

    検索の順番を変更

--- lib/ymn_tool_web/live/search_live/index.ex
+++ lib/ymn_tool_web/live/search_live/index.ex
@@ -24,16 +24,16 @@ defmodule YmnToolWeb.SearchLive.Index do
 
   defp assign_links(socket, q) do
     [
+      set_url("https://hexdocs.pm/elixir/search.html?q=", q, "hexdocs.pm/elixir"),
+      set_url("https://www.google.com/search?q=site%3Ahexdocs.pm+", q, "hexdocs.pm全体"),
       set_url("https://qiita.com/search?q=Elixir+", q, "Qiita"),
       set_url("https://www.google.com/search?q=Elixir+", q, "Google"),
-      set_url("https://hexdocs.pm/elixir/search.html?q=", q, "hexdocs.pm/elixir"),
       set_url(
         "https://translate.google.com/?hl=ja&tab=wT&sl=en&tl=ja&op=translate&text=",
         q,
         "Google翻訳"
       ),
       set_url("https://www.google.com/search?q=site%3Aelixirschool.com+", q, "elixirschool.com"),
-      set_url("https://www.google.com/search?q=site%3Ahexdocs.pm+", q, "hexdocs.pm全体"),
       %{
         url: "https://github.com/search?q=#{q}+language%3AElixir+&type=code",
         text: "github.comコード"
