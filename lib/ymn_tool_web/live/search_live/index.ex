56a0ef2b6c8e8c0f0771be7376d4779d2dbf6bad
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-20 12:09:09 +0900

    question_typeを追加

--- lib/ymn_tool_web/live/search_live/index.ex
+++ lib/ymn_tool_web/live/search_live/index.ex
@@ -8,34 +8,37 @@ defmodule YmnToolWeb.SearchLive.Index do
     IO.inspect(params)
 
     q = Map.get(params, "q", "")
+    question_type = Map.get(params, "question_type", "Elixir")
 
     socket
     |> assign(q: q)
-    |> assign_links(q)
-    |> assign_prompt(q)
+    |> assign(question_type: question_type)
+    |> assign_links(q ,question_type)
+    |> assign_prompt(q, question_type)
     |> then(&{:ok, &1})
   end
 
   @impl true
-  def handle_event("change", %{"q" => q}, socket), do: update_links(socket, q)
-  def handle_event("submit", %{"q" => q}, socket), do: update_links(socket, q)
-  def handle_event("clear-click", _, socket), do: update_links(socket, "")
+  def handle_event("change", %{"q" => q, "question_type" => question_type}, socket), do: update_links(socket, q, question_type)
+  def handle_event("submit", %{"q" => q, "question_type" => question_type}, socket), do: update_links(socket, q, question_type)
+  def handle_event("clear-click", _, socket), do: update_links(socket, "", "")
 
-  defp update_links(socket, q) do
+  defp update_links(socket, q, question_type) do
     socket
     |> assign(q: q)
-    |> assign_links(q)
-    |> assign_prompt(q)
+    |> assign(question_type: question_type)
+    |> assign_links(q, question_type)
+    |> assign_prompt(q, question_type)
     |> then(&{:noreply, &1})
   end
 
-  defp assign_links(socket, q) do
-    SearchLink.get(q)
+  defp assign_links(socket, q, question_type) do
+    SearchLink.get(q, question_type)
     |> then(&assign(socket, links: &1))
   end
 
-  defp assign_prompt(socket, q) do
-    LlmTemplates.get(q)
+  defp assign_prompt(socket, q, question_type) do
+    LlmTemplates.get(q, question_type)
     |> then(&assign(socket, prompt: &1))
   end
 end
