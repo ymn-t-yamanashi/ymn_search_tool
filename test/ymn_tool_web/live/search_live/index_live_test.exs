a480ba9fd08bf1dac9eab3abda5ae82bf376ee7f
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-23 08:50:48 +0900

    YmnToolWeb.SearchLive.IndexTest修正

--- test/ymn_tool_web/live/search_live/index_live_test.exs
+++ test/ymn_tool_web/live/search_live/index_live_test.exs
@@ -3,7 +3,32 @@ defmodule YmnToolWeb.SearchLive.IndexTest do
   import Phoenix.LiveViewTest
 
   test "index", %{conn: conn} do
-    {:ok, _lv, html} = live(conn, ~p"/")
+    {:ok, lv, html} = live(conn, ~p"/")
     assert html =~ "検索ワード"
+
+    # 条件取り込み方法
+    assert lv |> has_element?("#clear", "クリア(c)")
+    assert lv |> has_element?("#getclip", "クリックボードから取得(p)")
+    assert lv |> has_element?("#recognition", "音声認識(r)")
+
+    # 条件
+    assert lv |> has_element?("#q")
+    assert lv |> has_element?("#question_type", "システム開発")
+    assert lv |> has_element?("#llm_type", "質問")
+
+    # 検索ページ
+    assert lv |> has_element?("#link1", "hexdocs.pm/elixirで検索(1)")
+    assert lv |> has_element?("#link8", "github.com Elixirコードで検索(8)")
+
+    # LLM関連
+    assert lv |> has_element?("#gemini", "geminiを起動(g)")
+    assert lv |> has_element?("#llm", "Elixirについて質問")
+
+    # 用語リスト
+    assert lv
+           |> has_element?(
+             "div a.w-80.text-sm.rounded-lg.bg-cyan-800.px-2.m-1.text-white",
+             "GKEにElixir環境構築"
+           )
   end
 end
