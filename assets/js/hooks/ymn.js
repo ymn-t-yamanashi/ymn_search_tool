84615208be4bc26b63abb53a212302ca7d1c7db6
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-22 17:25:36 +0900

    addkeydownAndOnclick修正

--- assets/js/hooks/ymn.js
+++ assets/js/hooks/ymn.js
@@ -11,14 +11,13 @@ export const Ymn = {
 
 const init = () => {
   addkeydownAndOnclick("#getclip", "p", readText)
-  document.querySelector("#gemini").onclick = copy
-
+  addkeydownAndOnclick("#gemini", "g", copy)
+  
   document.addEventListener('keydown', (event) => {
     focus = document.querySelector("textarea:focus-visible, input:focus-visible")
     links = document.querySelectorAll("#links a")
 
     if (focus != null) return
-    keyAssign(event, "g", "gemini")
     keyAssign(event, "c", "clear")
     for (i = 1; i <= links.length; i++) {
       keyAssign(event, i, "link" + i)
--- assets/js/hooks/ymnlib.js
+++ assets/js/hooks/ymnlib.js
@@ -1,14 +1,15 @@
 const focus = document.querySelector("textarea:focus-visible, input:focus-visible")
 let keyList = [] 
 export const addkeydownAndOnclick = (query, key, fn) => {
-    document.querySelector(query).onclick = fn
-    keyList.push({key: key, fn: fn})
+    dom = document.querySelector(query)
+    dom.onclick = fn
+    keyList.push({key: key, dom: dom})
     console.log(keyList)
     document.addEventListener('keydown', (event) => {
         if (focus != null) return
         for(let i = 0; i < keyList.length; i++) {
             key = keyList[i]
-            if (event.key == key.key) key.fn()
+            if (event.key == key.key) key.dom.click()
         }
     })
 }
