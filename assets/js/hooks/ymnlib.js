c0f8167f482d82dea939bbc9134c2b1b410b4f8d
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-22 17:14:18 +0900

    addkeydownAndOnclick修正

--- assets/js/hooks/ymnlib.js
+++ assets/js/hooks/ymnlib.js
@@ -1,8 +1,14 @@
 const focus = document.querySelector("textarea:focus-visible, input:focus-visible")
+let keyList = [] 
 export const addkeydownAndOnclick = (query, key, fn) => {
     document.querySelector(query).onclick = fn
+    keyList.push({key: key, fn: fn})
+    console.log(keyList)
     document.addEventListener('keydown', (event) => {
         if (focus != null) return
-        if (event.key == key) fn()
+        for(let i = 0; i < keyList.length; i++) {
+            key = keyList[i]
+            if (event.key == key.key) key.fn()
+        }
     })
 }
