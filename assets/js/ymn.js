dc2ef0eec96dee7dc3b4881de29c73c1b5d06e8f
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-22 10:10:41 +0900

    js リファクタリング

--- assets/js/ymn.js
+++ assets/js/ymn.js
@@ -1,62 +1,61 @@
 export const hooks = {
-    ymn: {
-      mounted() {
-        init()
-      },
-      updated() {
-        init()
-      },
+  ymn: {
+    mounted() {
+      init()
     },
-  };
-
-function init() {
-    SpeechRecognition = webkitSpeechRecognition || SpeechRecognition
-    const recognition = new SpeechRecognition()
-
-    recognition.onresult = (event) => {
-        result = event.results[0][0].transcript
-        q = document.querySelector("#q")
-        q.value = result
-        document.querySelector("form").requestSubmit()
-    }
-    
-    document.querySelector("#recognition").onclick = recognition_start
-    document.querySelector("#getclip").onclick = readText
-    document.querySelector("#gemini").onclick = copy
-
-    document.addEventListener('keydown', (event) => {
-        focus = document.querySelector("textarea:focus-visible, input:focus-visible")
-        links = document.querySelectorAll("#links a")
-
-        if (focus != null) return 
-        keyAssign(event, "g", "gemini")
-        keyAssign(event, "c", "clear")
-        for(i = 1; i <= links.length; i++) {
-            keyAssign(event, i, "link" + i)
-        }
-        if(event.key == "r") recognition_start()
-    });
-
-    function recognition_start(){
-      recognition.start()
-    }
+    updated() {
+      init()
+    },
+  },
 }
 
-function copy(){
-  let llm = document.querySelector("#llm")
-  let value = llm.value
+const init = () => {
+  SpeechRecognition = webkitSpeechRecognition || SpeechRecognition
+  const recognition = new SpeechRecognition()
 
-  navigator.clipboard.writeText(value)
-} 
+  recognition.onresult = (event) => {
+    result = event.results[0][0].transcript
+    q = document.querySelector("#q")
+    q.value = result
+    document.querySelector("form").requestSubmit()
+  }
 
-function readText(){
-  let questionType = document.querySelector("#question_type")
-   navigator.clipboard.readText()
-    .then(text => {
-      location.href="/?q=" + text + "&question_type=" + questionType.value
+  const recognition_start = () => {
+    recognition.start()
+  }
+
+  document.querySelector("#recognition").onclick = recognition_start
+  document.querySelector("#getclip").onclick = readText
+  document.querySelector("#gemini").onclick = copy
+
+  document.addEventListener('keydown', (event) => {
+    focus = document.querySelector("textarea:focus-visible, input:focus-visible")
+    links = document.querySelectorAll("#links a")
+
+    if (focus != null) return
+    keyAssign(event, "g", "gemini")
+    keyAssign(event, "c", "clear")
+    for (i = 1; i <= links.length; i++) {
+      keyAssign(event, i, "link" + i)
+    }
+    if (event.key == "r") recognition_start()
   })
+
 }
 
-function keyAssign(event, key, id) {
-  if(event.key == key) document.querySelector("#" + id).click()
+const copy = () => {
+  let llm = document.querySelector("#llm")
+  navigator.clipboard.writeText(llm.value)
+}
+
+const readText = () => {
+  let questionType = document.querySelector("#question_type")
+  navigator.clipboard.readText()
+    .then(text => {
+      location.href = "/?q=" + text + "&question_type=" + questionType.value
+    })
+}
+
+const keyAssign = (event, key, id) => {
+  if (event.key == key) document.querySelector("#" + id).click()
 }
