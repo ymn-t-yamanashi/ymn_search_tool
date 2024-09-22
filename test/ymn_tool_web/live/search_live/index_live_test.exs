7bc94f33b395466bbf9054894fbe769f887c320b
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-23 01:07:59 +0900

    YmnToolWeb.SearchLive.IndexTest作成

---
+++ test/ymn_tool_web/live/search_live/index_live_test.exs
@@ -0,0 +1,10 @@
+defmodule YmnToolWeb.SearchLive.IndexTest do
+  use YmnToolWeb.ConnCase
+  import Phoenix.LiveViewTest
+
+  test "index",  %{conn: conn} do
+    {:ok, _lv, html} = live(conn, ~p"/")
+    assert html =~ "検索ワード"
+  end
+
+end
