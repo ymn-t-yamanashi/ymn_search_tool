d49d2a5921574ef5d48374b50816a9b1d3177450
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-22 11:46:45 +0900

    hooksリファクタリング

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
-import {hooks} from "./index.js"
+import {hooks} from "./hooks"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {longPollFallbackMs: 2500,params: {_csrf_token: csrfToken}, hooks})
