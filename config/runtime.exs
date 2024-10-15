09c72c39aec7ce3f5361cbb7c9047d80dcb742a9
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-10-15 23:03:46 +0900

    host変更

--- config/runtime.exs
+++ config/runtime.exs
@@ -37,13 +37,13 @@ if config_env() == :prod do
   #     You can generate one by calling: mix phx.gen.secret
   #     """
 
-  # host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "tool.ymnbuild.org"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :ymn_tool, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :ymn_tool, YmnToolWeb.Endpoint,
-    # url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
